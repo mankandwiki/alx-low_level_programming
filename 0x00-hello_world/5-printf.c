48a1101cc9d0f542b996893038a8a5f128445a91
mankandwiki <mankalilian1@gmail.com>
2023-04-25 01:09:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,5 @@
  */
 int main(void)
 {
-        puts("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 }
